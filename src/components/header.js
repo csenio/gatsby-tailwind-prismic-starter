653eaf2ae81824ffa4fbb51862338a97841da936
jescowuester <jesco@storyofams.com>
2020-02-09 12:32:01 +0700

    fix: lint

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,8 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import {Link} from 'gatsby'
+import PropTypes from 'prop-types'
+import React from 'react'
 
-const Header = ({ siteTitle }) => (
+const Header = ({siteTitle}) => (
   <header
     style={{
       background: `rebeccapurple`,
@@ -16,7 +16,7 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1 style={{margin: 0}}>
         <Link
           to="/"
           style={{
